229057066057088956f884f5552cc24d08bf5bbe
Daniel Pontasch <pontasch.daniel@gmail.com>
2026-09-10 07:30:55 +0200

    mobile Ansicht

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
-const CACHE_NAME = "dp-jagd-shell-v194";
+const CACHE_NAME = "dp-jagd-shell-v196";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.json?v=2",
   "./assets/app-icon.svg?v=2",
+  "./assets/apple-touch-icon.png?v=1",
   "./assets/rechnung-logo.png",
-  "./css/layout.css?v=203",
+  "./css/layout.css?v=204",
   "./css/sidebar.css?v=168",
   "./css/login.css",
   "./css/orte.css?v=160",
